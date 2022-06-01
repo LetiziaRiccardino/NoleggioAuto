aafb4663b083a9f277015f913a2d5c162f45b54f
daviricki <74833327+daviricki@users.noreply.github.com>
2022-06-01 11:47:30 +0200

    Lezione 9 maggio 2022

---
+++ src/main/java/it/polito/tdp/noleggio/model/TestSimulatore.java
@@ -0,0 +1,14 @@
+package it.polito.tdp.noleggio.model;
+
+public class TestSimulatore {
+
+	public static void main(String[] args) {
+		Simulatore sim= new Simulatore(6);
+		
+		sim.caricaEventi();
+		sim.run();
+		
+		System.out.println("Clienti: "+sim.getnClientiTot()+" di cui "+sim.getnClientiInsoddisfatti()+" insoddisfatti");
+	}
+
+}
